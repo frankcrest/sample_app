4c41e3fe203f25fef213967bbf56771ed837e420
frankcrest <frankcrestview@hotmail.com>
2015-05-27 22:44:51 -0400

    Finish login/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
  Rails.application.routes.draw do
 
 
+  get 'sessions/new'
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
   get 'users/new'
 
